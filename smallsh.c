65fe66bfb9206bf0d0866c331048e8f05b622158
JakeNathe <Jakenathe1@gmail.com>
2023-11-09 14:59:33 -0600

    added child process handlers and signal handlers

--- smallsh.c
+++ smallsh.c
@@ -7,20 +7,36 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
+#include <signal.h>
+#include <inttypes.h>
 
 #ifndef MAX_WORDS
 #define MAX_WORDS 512
 #endif
 
-char *backgroundPid = "";
-char *foregroundPid = "0";
+char *background_pid = "";  // $!
+char *foreground_pid = "0";  // $?
+int background_flag = 0;
+int background_child = 0;
+int childExitStatus = 0;
+int expanded_param_flag = 0;
 int nwords = 0;
-int expanded_param = 0;
+char * input_file = NULL;
+char * output_file = NULL;
+int inputFD = 0;
+int outputFD = 0;
 
 char *words[MAX_WORDS];
 size_t wordsplit(char const *line);
 char * expand(char const *word);
 
+// Ignore SIGINT
+void sigint_handler(int signo) {}
+
+// Ignore SIGTSTP
+void sigtstp_handler(int signo){}
+
+/* MAIN FUNCTION */
 int main(int argc, char *argv[])
 {
   // open input to std in if no args. If arg given, open that file, if 2+ args error
@@ -34,17 +50,42 @@ int main(int argc, char *argv[])
     errx(1, "Too many arguments\n");
   }
 
+  // Initialize line and n 
   char *line = NULL;
   size_t n = 0;
+
+  // MAIN LOOP
   for (;;) {
-prompt:
+start:
+    // default settings
+    expanded_param_flag = 0;
+    input_file = NULL;
+    output_file = NULL;
+    // ignore signals
+    signal(SIGINT, sigint_handler);
+    signal(SIGTSTP, sigtstp_handler);
     // clear prev array
     memset(words, 0, sizeof(words));
     
-    /* TODO: Manage background processes LATER....*/
-
+    /* 
+      Manage background processes 
+    */
+    // check if process has completed. WNOHANG doesn't block the parent process
+    while ((background_child = waitpid(0, &childExitStatus, WUNTRACED | WNOHANG)) > 0) {
+      // if greater than 0 a child process has completed. 
+      if (WIFEXITED(childExitStatus)) {
+        fprintf(stderr, "Child process %jd done. Exit status %d.\n", (intmax_t) background_child, WEXITSTATUS(childExitStatus));
+      }
+      if (WIFSTOPPED(childExitStatus)) {
+        kill(background_child, SIGCONT);
+        fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) background_child);
+      }
+      if (WIFSIGNALED(childExitStatus)) {
+        fprintf(stderr, "Child process %jd done. Signaled %d.\n", (intmax_t) background_child, WTERMSIG(childExitStatus));
+      }
+    }
     
-    /* TODO: prompt */
+    /* Interactive mode Prompt */
     if (input == stdin) {
       char *PS1 = getenv("PS1");
       if (PS1 == NULL){
@@ -57,38 +98,66 @@ prompt:
     ssize_t line_len = getline(&line, &n, input);
     // exit if file is at end
     if ((line_len < 0) && (feof(input))) {
-      exit(atoi(foregroundPid));
+      exit(atoi(foreground_pid));
     }
     if (line_len < 0) err(1, "%s", input_fn);  
 
-
+    
+    // split and expand words
     size_t nwords = wordsplit(line);
     for (size_t i = 0; i < nwords; ++i) {
       //fprintf(stderr, "Word %zu: %s\n", i, words[i]);
       char *exp_word = expand(words[i]);
       free(words[i]);
       words[i] = exp_word;
-      if (expanded_param == 1){
+      if (expanded_param_flag == 1){
         // get env value
         char *new_word = getenv(words[i]);
         if (new_word == NULL){
           // set to empty str if null
           new_word = "";
         }
+        // replace word with expanded param
         free(words[i]);
         words[i] = new_word;
-        expanded_param = 0;
       }
       //fprintf(stderr, "Expanded Word %zu: %s\n", i, words[i]);
     }
 
-    // which command? cd, exit, or other
+    // ignore all below code if no words were entered
+    if (nwords == 0) {
+      goto start;
+    }
+
+    /* 
+      Check for special characters &, <, >, >>.
+    */
+    // check if it should be a background porocess by & at end of line
+    if (strcmp(words[nwords-1], "&") == 0) {
+      background_flag = 1;
+      free(words[nwords-1]);
+      words[nwords -1] = NULL;
+      // decrement for word that was removed
+      nwords--;
+    } else {
+      background_flag = 0;
+    }
+
+    // check for redirection <, >, or >> as a word
+    // check for output redirection.
+    if ((nwords > 2) && (strcmp(words[nwords-1], "<") == 0)) {
+      //
+    }
+
+    /* 
+    HANDLE COMMANDS. exit, cd, or all others
+    */
     // case 1: exit
     if (strcmp(words[0], "exit") == 0) {
       if (nwords > 2){
         // only one exit arg allowed
         fprintf(stderr, "Too many arguments\n");
-        goto prompt;
+        goto start;
       } else if (nwords == 2){
         // exit command is given
           if (isdigit(*words[1])) {
@@ -96,31 +165,30 @@ prompt:
           } else {
             // exit arg is not an int
             fprintf(stderr, "Exit command is not an int\n");
-            goto prompt;
+            goto start;
         }} else {
-          // not exit arg given. exit default
-          exit(atoi(foregroundPid));
+          // no exit arg given. exit default
+          exit(atoi(foreground_pid));
         }
     // case 2: cd
     } else if (strcmp(words[0], "cd") == 0) {
         if (nwords == 2) {
           chdir(words[1]);
-          goto prompt;
+          goto start;
         } else if (nwords == 1) {
           chdir(getenv("HOME"));
         } else {
           fprintf(stderr, "Too many arguments\n");
-          goto prompt;
+          goto start;
         }
     // case 3: other command
     } else {
-      // Execute the command
       fflush(stdout);
-      pid_t spawnPid = -5;
+      pid_t spawn_pid = -5;
       int childExitStatus = -5;
 
-      spawnPid = fork();
-      switch (spawnPid) {
+      spawn_pid = fork();
+      switch (spawn_pid) {
         case -1: {
           perror("Fork error\n"); 
           exit(1); 
@@ -134,15 +202,23 @@ prompt:
         }
         default: {
           // Parent process
-          // wait for child to process command
-          waitpid(spawnPid, &childExitStatus, 0);
+          // wait for child to process command if foreground
+          if (background_flag == 0) {
+            spawn_pid = waitpid(spawn_pid, &childExitStatus, 0);
+            foreground_pid = malloc(sizeof(int) * 8);
+            sprintf(foreground_pid, "%d", WEXITSTATUS(childExitStatus));
+          } else {
+            // background
+            background_pid = malloc(sizeof(int) * 8);
+            sprintf(background_pid, "%d", WEXITSTATUS(childExitStatus));
+          }
         }
       }
     }
   }
 } 
 
-char *words[MAX_WORDS] = {0};
+char *words[MAX_WORDS] = {0}; // is this needed??? from starter code
 
 
 /* Splits a string into words delimited by whitespace. Recognizes
@@ -263,14 +339,14 @@ expand(char const *word)
   build_str(NULL, NULL);
   build_str(pos, start);
   while (c) {
-    if (c == '!') build_str(backgroundPid, NULL);
+    if (c == '!') build_str(background_pid, NULL);
     else if (c == '$') build_str(pid, NULL);
-    else if (c == '?') build_str(foregroundPid, NULL);
+    else if (c == '?') build_str(foreground_pid, NULL);
     else if (c == '{') {
       build_str("", NULL);
       build_str(start + 2, end - 1);
       build_str("", NULL);
-      expanded_param = 1;
+      expanded_param_flag = 1;
     }
     pos = end;
     c = param_scan(pos, &start, &end);
